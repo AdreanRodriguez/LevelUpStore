7dbfac482e29e35f53008110729ea08b9655d9b4
AdreanRodriguez <loco_bladare@hotmail.com>
2024-12-30 12:36:17 +0100

    working

---
+++ levelupstore/src/app/genres/page.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { fetchGenres } from "../lib/fetcher";
+
+export default async function GenresPage() {
+  try {
+    const genres = await fetchGenres();
+
+    return (
+      <div>
+        <h1>Genres</h1>
+        <ul>
+          {genres.results.map((genre) => (
+            <li key={genre.id}>
+              <Link href={`/genres/${genre.id}`}>{genre.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  } catch (error: any) {
+    return <div>Error loading genres: {error.message}</div>;
+  }
+}
--- levelupstore/src/app/lib/fetcher.ts
+++ levelupstore/src/app/lib/fetcher.ts
@@ -1,28 +1,29 @@
 import { Product } from "@/app/types/product";
 import { ProductListResponse } from "@/app/types/product";
+import { Genres, GenresListResponse } from "@/app/types/genres";
 
 const API_KEY = process.env.NEXT_PUBLIC_RAWG_API_KEY;
+const API_URL_GAMES = "https://api.rawg.io/api/games";
+const API_URL_GENRES = "https://api.rawg.io/api/genres";
 
-// Funktion för att hämta produkter från RAWG API
+// Hämta antal produkter (page_size=50 som blir 50 produkter)
 export async function fetchProductsFromAPI(): Promise<ProductListResponse> {
   if (!API_KEY) {
     throw new Error("Missing API key");
   }
 
-  const response = await fetch(
-    `https://api.rawg.io/api/games?key=${API_KEY}&page_size=50&ordering=-added`
-  );
+  const response = await fetch(`${API_URL_GAMES}/?key=${API_KEY}&page_size=50&ordering=-added`);
 
   if (!response.ok) {
     throw new Error(`Failed to fetch products: ${response.status}`);
   }
 
-  return response.json();
+  const data: ProductListResponse = await response.json();
+  return data;
 }
 
+// Hämta specifik produkt på id
 export async function fetchProductById(id: string): Promise<Product> {
-  const API_KEY = process.env.NEXT_PUBLIC_RAWG_API_KEY;
-
   if (!API_KEY) {
     throw new Error("Missing API key. Ensure NEXT_PUBLIC_RAWG_API_KEY is set in your environment.");
   }
@@ -31,17 +32,53 @@ export async function fetchProductById(id: string): Promise<Product> {
     throw new Error("Product ID is required to fetch the product.");
   }
 
-  const response = await fetch(`https://api.rawg.io/api/games/${id}?key=${API_KEY}`);
+  const response = await fetch(`${API_URL_GAMES}/${id}?key=${API_KEY}`);
 
   if (!response.ok) {
     throw new Error(`Failed to fetch product with ID ${id}: ${response.status}`);
   }
 
-  const data = await response.json();
+  const data: Product = await response.json();
 
   if (!data || !data.id) {
     throw new Error("Invalid product data received from the API.");
   }
 
+  return data;
+}
+
+// Hämta alla kategorier
+export async function fetchGenres(): Promise<GenresListResponse> {
+  if (!API_KEY) {
+    throw new Error("Missing API key. Ensure NEXT_PUBLIC_RAWG_API_KEY is set.");
+  }
+
+  const response = await fetch(`${API_URL_GENRES}?key=${API_KEY}&page_size=10&ordering=-added`);
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch genres: ${response.status}`);
+  }
+
+  const data: GenresListResponse = await response.json();
+  return data;
+}
+
+// Hämta kategori baserat på id
+export async function fetchGenreById(id: string): Promise<Genres> {
+  if (!API_KEY) {
+    throw new Error("Missing API key. Ensure NEXT_PUBLIC_RAWG_API_KEY is set.");
+  }
+
+  if (!id) {
+    throw new Error("Genre ID is required to fetch the genres.");
+  }
+
+  const response = await fetch(`${API_URL_GENRES}/${id}?key=${API_KEY}`);
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch genre with ID ${id}: ${response.status}`);
+  }
+
+  const data: Genres = await response.json();
   return data;
 }
